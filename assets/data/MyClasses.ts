58da0840a90d26635cb06a8e5080565f8be5c576
nguvanha <52674146+nguvanha@users.noreply.github.com>
2019-07-29 12:20:30 -0400

    Add files via upload

---
+++ assets/data/MyClasses.ts
@@ -0,0 +1,9 @@
+import { nguvanha } from '../../app/nguvanha'
+
+export const MYINFO: nguvanha = {
+    snumber: 991497440, 
+    sname: 'Van Ha Nguyen', 
+    slogin: 'nguvanha',
+    scampus: 'Davis Campus',
+    assigntitle: 'Assignment 3'
+}
---
+++ assets/data/MySong.ts
@@ -0,0 +1,32 @@
+import { songInfo } from '../../app/song'
+
+export const MYSONGS: songInfo[] = [
+    {
+        songName: 'Love It If We Made It', 
+        artist: 'The 1975', 
+        genre: 'Electropop, funk', 
+        releasedYr: 2018, 
+        picture: 'the1975.png'
+    },
+    {
+        songName: 'Hall of Fame', 
+        artist: 'The Scipt ft. will.i.am', 
+        genre: 'Pop rock, hip hop', 
+        releasedYr: 2012, 
+        picture: 'halloffame.jpg'
+    },
+    {
+        songName: 'Señorita', 
+        artist: 'Shawn Mendes and Camila Cabello', 
+        genre: 'Latin', 
+        releasedYr: 2019, 
+        picture: 'senorita.png'
+    },
+    {
+        songName: 'Fire', 
+        artist: 'Barns Courtney', 
+        genre: 'Blues rock', 
+        releasedYr: 2015, 
+        picture: 'fire.jpg'
+    }
+]
